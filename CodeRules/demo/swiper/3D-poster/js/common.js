d81159e314df11739bb1c2d1b242aaddd7dbf3f2
liaoxb <liaoxb@jxsg.com>
2019-09-26 18:33:21 +0800

    demos

---
+++ CodeRules/demo/swiper/3D-poster/js/common.js
@@ -0,0 +1,16 @@
+// 页面乱转规则
+/**
+ *  样式规则 rem =  实际大小 / 50 =  (x)rem
+ */
+var i = 0;
+i = window.devicePixelRatio > 1 ? 1 / devicePixelRatio : 1;
+document.write('<meta name="viewport" content="width=device-width,initial-scale=' + i + ',minimum-scale=' + i + ',maximum-scale=' + i + '">');
+window.addEventListener('resize', pageSize);
+window.addEventListener('load', pageSize);
+pageSize();
+
+function pageSize() {
+  var ohtml = document.getElementsByTagName('html')[0];
+  var iWidth = ohtml.getBoundingClientRect().width || document.documentElement.clientWidth;
+  ohtml.style.fontSize = iWidth / 15 + 'px';
+}
